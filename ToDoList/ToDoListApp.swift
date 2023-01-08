d83eaa8e01640338e0040316bd933f9160843684
Dmitry Sharabin <dmitrysharabin@gmail.com>
2023-01-08 23:05:40 +0100

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Dmitry Sharabin on 8.1.23..
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
